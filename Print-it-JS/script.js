96a6c2dd928701c57c8ace8b57ba6d4d08cb96ed
Beugnies Jimmy <93620368+Zrtehhz@users.noreply.github.com>
2023-06-21 13:23:52 +0200

    Fini

--- Print-it-JS/script.js
+++ Print-it-JS/script.js
@@ -21,19 +21,37 @@ const slides = [
 
 // si c'est un int (float) ou une value, qui recupère dans mon code html la balise ou l'id/class
 
+
 	let chiffre = 0;
-	let dots = document.querySelector('.dots');
-	let dot = dots.querySelector('.dot');
+	const dots = document.querySelectorAll('.dot');
 	let paragraphe = document.querySelector('p');
 	let bannerImg = document.querySelector('#banner-img');
 	let flecheGauche = document.querySelector('.arrow_left');
 	let flecheDroite = document.querySelector('.arrow_right');
 
 
+// ma fonction va m'être à jour ma dots comme ceci : Comme j'ai défini ma constante dots
+// forEach va parcourir chaque .dot dans mon main .dots
+//pour chaque .dot ma fonction vérifie si son index correspond à la valeur de chiffre qui est égal à 0.
+
+// Si l'index correspond à 0 alors l'élément .dot est actif, dans ce cas je change le background color.
+// et si à l'inverse il ne correspond pas alors il reste non actif et donc au background color de base.
+
+
+function updateDots() {
+	dots.forEach((dot, index) => {
+		if (index === chiffre) {
+		  dot.style.backgroundColor = 'white'; 
+		} else {
+		  dot.style.backgroundColor = 'gray'; 
+		}
+	  });
+}
+
+
 // Je défini ma fonction, lui ajoute un nom Changement de slide ainsi qu'un argument "sens"
 // Si 0 >= à mon slides.length alors il renvoi 0 et si inversement il renvoi 1
 // Si ma slide correspond à 0 alors il change d'images et de paragraphe
-
 	function ChangementDeSlide(sens) {
 		  chiffre = chiffre + sens;
  		 if (chiffre >= slides.length)
@@ -45,6 +63,9 @@ const slides = [
 				bannerImg.src = slides[chiffre].image;
 				paragraphe.innerHTML = slides[chiffre].tagLine;
 			  }
+
+			  updateDots();
+
 	}
 
 // J'y ajoute un eventListener sur mes flèches, au clic de souris, 
@@ -59,4 +80,6 @@ const slides = [
   		console.log('la flèche de droite fonctionne bien');
   		ChangementDeSlide(1);
 	});
+
+
 
