67f4d73dc3486dd2c9c075709d7b8ab3b9e0b861
Beugnies Jimmy <93620368+Zrtehhz@users.noreply.github.com>
2023-06-29 14:49:57 +0200

    .

--- script.js
+++ script.js
@@ -19,7 +19,9 @@
 
 	// Je defini avec let (= à var) mes variables ainsi que ce qu'elle correspondent
 
-	// si c'est un int (float) ou une value, qui recupère dans mon code html la balise ou l'id/class
+	// si c'est un int (float) ou une value.
+
+	// Sinon je demande à ce que il récupère ma balise ou ma classe/id
 
 
 		let chiffre = 0;
@@ -30,29 +32,39 @@
 		let flecheDroite = document.querySelector('.arrow_right');
 
 
-	// ma fonction va m'être à jour ma dots comme ceci : Comme j'ai défini ma constante dots
-	// forEach va parcourir chaque .dot dans mon main .dots
-	//pour chaque .dot ma fonction vérifie si son index correspond à la valeur de chiffre qui est égal à 0.
-
-	// Si l'index correspond à 0 alors l'élément .dot est actif, dans ce cas je change le background color.
-	// et si à l'inverse il ne correspond pas alors il reste non actif et donc au background color de base.
-
+		// Ma fonction updateDots fait ceci : 
+		// Elle itère sur chaque élément dot du tableau dots en utilisant la méthode forEach.
+		// Pour chaque dot, la fonction vérifie si son index correspond à la valeur de chiffre :
+		// Si l'index de l'élément dot est égal à la valeur de chiffre, alors la classe CSS 'active' est ajoutée à cet élément en utilisant dot.classList.add('active'). 
+		// Et inversement, si l'élément dot n'est pas égal à la valeur chiffre alors la classe CSS 'active' est supprimé.
 
 	function updateDots() {
 		dots.forEach((dot, index) => {
 			if (index === chiffre) {
-				dot.classList.add('active'); // Ajoute la classe active
+				dot.classList.add('active'); 
 			} else {
-			  dot.classList.remove('active'); // Supprime la classe active
+			  dot.classList.remove('active'); 
 			}
 		});
 	}
 
 
-	// Je défini ma fonction, lui ajoute un nom Changement de slide ainsi qu'un argument "sens"
-	// Si 0 >= à mon slides.length alors il renvoi 0 et si inversement il renvoi 1
-	// Si ma slide correspond à 0 alors il change d'images et de paragraphe
-		function ChangementDeSlide(sens) {
+// chiffre += sens; ajoute la valeur de sens à la variable chiffre. Cela permet de changer l'index actuel de la diapositive en fonction de la valeur de sens.
+//   Si la valeur de chiffre est supérieure ou égale à la longueur du tableau slides, cela signifie que l'index dépasse le nombre de diapositives disponibles.
+// Dans ce cas, chiffre est réinitialisé à 0, ce qui permet de boucler et de revenir à la première diapositive.
+
+//Si la valeur de chiffre est inférieure à 0, cela signifie que l'index est négatif. Dans ce cas, chiffre est réinitialisé à slides.length - 1,
+// ce qui permet de boucler et de revenir à la dernière diapositive.
+// Si une diapositive existe à l'index chiffre dans le tableau slides, alors les actions suivantes sont effectuées :
+
+//met à jour la source de l'image dans un élément du DOM avec l'URL de l'image de la diapositive correspondante.
+
+//met à jour le contenu HTML d'un élément du DOM avec la ligne de balise (tagline) de la diapositive correspondante.
+
+//Enfin, la fonction updateDots() est appelée pour mettre à jour les indicateurs (dots) des diapositives, exemple : image 1 signifie que ma dot sera placé en 1 sur 4.
+
+
+function ChangementDeSlide(sens) {
 			chiffre += sens;
 			if (chiffre >= slides.length)
 			chiffre = 0;
@@ -69,12 +81,16 @@
 		}
 
 	// J'y ajoute un eventListener sur mes flèches, au clic de souris, 
-	// dans la console, sa affiche mon message et sa change d'image et de paragraphe
+	// dans la console, sa affiche mon message et J'appelle ma fonction "ChangementDeSlide" avec l'argument -1. ( -1 signifie vers la gauche)
 
 		flecheGauche.addEventListener('click', function onClick() {
 			console.log('la flèche de gauche fonctionne bien');
 			ChangementDeSlide(-1);
 		});
+
+
+	// J'y ajoute un eventListener sur mes flèches, au clic de souris, 
+	// dans la console, sa affiche mon message et J'appelle ma fonction "ChangementDeSlide" avec l'argument 1. ( 1 signifie vers la droite)
 
 		flecheDroite.addEventListener('click', function onClick() {
 			console.log('la flèche de droite fonctionne bien');
